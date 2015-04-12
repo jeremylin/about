eb72efbea280a8f30c9d8415d3545647dd1cc81b
jeremy.lin <jeremy.lin@ifalo.com.tw>
2015-04-12 11:39:19 +0800

    Fix RWD layout

--- javascripts/tab.js
+++ javascripts/tab.js
@@ -1,19 +1,6 @@
 $(document).ready(function(){
 	$( ".slogan" ).hide().fadeIn( 2400 );
 
-	$(document).on('click', '.icon', function(){
-		var $element = $('.description'),
-			$this = $(this),
-			$iconHere = $('.icon.here');
-			section = $this.data('section');
-		
-		$iconHere.removeClass('here');
-		$this.addClass("here");
-		
-		$element.removeClass('content');
-		$('.description.' + section).addClass('content');
-	});
-
 	$(".Phone").click(function(){
 	    $(".callWrap").addClass("phoneCall");
 	    $( ".phoneCall" ).fadeIn( 500 );
